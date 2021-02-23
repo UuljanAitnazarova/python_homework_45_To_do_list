14e7de0e5c8c2c5534247ddd314ab5999cbaf3e7
uuljan <u.aitnazarova@gmail.com>
2021-02-23 17:37:50 +0600

    Added TaskAdmin class in admin.py

--- source/todoapp/admin.py
+++ source/todoapp/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-
 from todoapp.models import Task
 
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_completed']
+    list_filter = ['status']
+    search_fields = ['description']
+    fields = ['description', 'status', 'date_completed']
+
+admin.site.register(Task, TaskAdmin)
