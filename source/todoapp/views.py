a3a2ba865b08b1696d1c3ea84cef4b1e2f712cdd
uuljan <u.aitnazarova@gmail.com>
2021-02-23 18:50:48 +0600

    Implemented index_view, created the templete index.html

--- source/todoapp/views.py
+++ source/todoapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+
+
+def index_view(request):
+    tasks = Task.objects.all()
+    
+    return render(request, 'index.html', context={'tasks':tasks})
