e744592c6b247e9470e12531a487b747acfe377a
uuljan <u.aitnazarova@gmail.com>
2021-02-23 17:06:27 +0600

    Created todoapp, creadted model Task, made migrations for Task model

---
+++ source/todoapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('new', 'Новая'), 
+        ('in_progress', 'В процессе'),  
+        ('done', 'Сделано')
+        ]
+
+    description = models.CharField(max_length=200, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, null=False, blank=False, default='new', verbose_name='статус')
+    date_completed = models.DateField(blank=True, null=True, verbose_name='Дата выполнения')
+
+
+    def __str__(self):
+        return self.description
