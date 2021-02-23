390e1be128785228d943549e3cbf34d1048d632c
uuljan <u.aitnazarova@gmail.com>
2021-02-23 20:18:05 +0600

    Implemented create_task view, task_create.html, updated urls

--- source/todoapp/urls.py
+++ source/todoapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from todoapp.views import index_view
+from todoapp.views import index_view, task_view, task_create_view
 
 urlpatterns = [
     path('', index_view),
+    path('task/', task_view),
+    path('tasks/add/', task_create_view),
 ]
--- source/todoapp/views.py
+++ source/todoapp/views.py
@@ -5,5 +5,26 @@ from .models import Task
 
 def index_view(request):
     tasks = Task.objects.all()
-    
     return render(request, 'index.html', context={'tasks':tasks})
+
+def task_view(request):
+    task_id = request.GET.get('id')
+    task = Task.objects.get(id=task_id)
+    return render(request, 'task_view.html', context={'task': task})
+
+def task_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'task_create.html', {'status_choice': Task.STATUS_CHOICES})
+    elif request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        date_completed = request.POST.get('date_completed')
+        if not date_completed:
+            date_completed=None
+
+        task = Task.objects.create(
+            description=description,
+            status=status,
+            date_completed= date_completed
+        )
+        return render(request, 'task_view.html', context = {'task': task})
